1363cc71b01b1cc997f58b5d4721703511e950fc
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-07 15:07:56 +0500

    refactor: Make BookShelf a stateless component
    Make BookShelf as stateless functional component because it does not need to track any internal state

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 
-class BookShelf extends Component {
-    render() {
-        const { title, books, shelfTypes, onChangeBookShelf } = this.props
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{title}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {books && books.map(book => (
-                            <li key={`book_${book.id}`}>
-                                <Book
-                                    id={book.title}
-                                    title={book.title}
-                                    authors={book.authors}
-                                    shelf={book.shelf}
-                                    image={book.imageLinks.smallThumbnail}
-                                    shelfOptions={shelfTypes}
-                                    onChangeBookShelf={shelf => onChangeBookShelf(book, shelf)}
-                                />
-                            </li>
-                        ))}
-                    </ol>
-                </div>
+const BookShelf = ({
+    title,
+    books,
+    shelfTypes,
+    onChangeBookShelf
+    }) => (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books && books.map(book => (
+                        <li key={`book_${book.id}`}>
+                            <Book
+                                id={book.title}
+                                title={book.title}
+                                authors={book.authors}
+                                shelf={book.shelf}
+                                image={book.imageLinks.smallThumbnail}
+                                shelfOptions={shelfTypes}
+                                onChangeBookShelf={shelf => onChangeBookShelf(book, shelf)}
+                            />
+                        </li>
+                    ))}
+                </ol>
             </div>
-        )
-    }
-}
+        </div>
+    )
 
 export default BookShelf
