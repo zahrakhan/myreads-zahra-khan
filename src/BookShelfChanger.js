ace1d2a84e4e9f4706264b7d60b9371ab5d0713f
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-07 14:23:00 +0500

    feat: Add stateless BookShelfChanger component
    Add stateless BookShelfChanger component that takes in selected value, options as attributes and trigers a change in book's self

---
+++ src/BookShelfChanger.js
@@ -0,0 +1,18 @@
+export default function BookShelfChanger({
+    selectedValue,
+    options,
+    onChange,
+}) {
+    return (
+        <div className="book-shelf-changer">
+            <select value={selectedValue} onChange={event => onChange(event.target.value)}>
+                <option value="none" disabled>Move to...</option>
+                {Object.keys(options).map(option => (
+                    <option key={option} value={option}>
+                        {options[option]}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
