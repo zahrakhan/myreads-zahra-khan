3c0dcdc19d428a80b98c214f37c564fe4a1d3365
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-08 02:14:36 +0500

    feat: Define routes for my reads and book search

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import MyReads from './MyReads'
 import SearchBooks from './SearchBooks'
 
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-  navigateToSearch = () => {
-    this.setState({ showSearchPage: true })
-  }
-  navigateToShelf = () => {
-    this.setState({ showSearchPage: false })
-  }
-
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ?
-          <SearchBooks onClickBack={this.navigateToShelf} /> :
-          <MyReads onSearch={this.navigateToSearch} />
-        }
+      <div className='app'>
+        <Route exact
+          path='/'
+          render={() => (
+            <MyReads />
+          )}
+        />
+        <Route exact
+          path="/search"
+          render={() => (
+            <SearchBooks />
+          )}
+        />
       </div>
     )
   }
