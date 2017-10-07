6df6e3455bd5e570e7a659d6e90e2754cc4a94f0
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-07 15:11:05 +0500

    refactor: Make BookList a stateless component
    Make BookList a stateless functional component because it does not need to track any internal state

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import BookShelf from './BookShelf'
 
-class ListBooks extends Component {
-    render() {
-        let { shelves, shelfTypes, onSearch, onChangeBookShelf } = this.props
-        return (
-            <div className="list-books" >
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    {Object.keys(shelves).map(shelf => (
-                        <BookShelf
-                            key={shelf}
-                            title={shelfTypes[shelf]}
-                            books={shelves[shelf]}
-                            shelfTypes={shelfTypes}
-                            onChangeBookShelf={onChangeBookShelf}
-                        />
-                    ))}
-                </div>
-                <div className="open-search">
-                    <a onClick={() => onSearch()}>Add a book</a>
-                </div>
+const ListBooks = ({
+    shelves,
+    shelfTypes,
+    onSearch,
+    onChangeBookShelf
+}) => (
+        <div className="list-books" >
+            <div className="list-books-title">
+                <h1>MyReads</h1>
             </div>
-        )
-    }
-}
+            <div className="list-books-content">
+                {Object.keys(shelves).map(shelf => (
+                    <BookShelf
+                        key={shelf}
+                        title={shelfTypes[shelf]}
+                        books={shelves[shelf]}
+                        shelfTypes={shelfTypes}
+                        onChangeBookShelf={onChangeBookShelf}
+                    />
+                ))}
+            </div>
+            <div className="open-search">
+                <a onClick={() => onSearch()}>Add a book</a>
+            </div>
+        </div>
+    )
 
 export default ListBooks
