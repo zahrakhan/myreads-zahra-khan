61de3e648c41b7e037b71273486f9b476437d975
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-08 01:39:24 +0500

    feat: Add higher order component to maintain book state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import ListBooks from './ListBooks'
+import MyReads from './MyReads'
 import SearchBooks from './SearchBooks'
-import { groupItems } from './Utils'
+
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -13,27 +12,7 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false,
-    shelves: {},
-    shelfTypes: {
-      'currentlyReading': 'Currently Reading',
-      'wantToRead': 'Want to Read',
-      'read': 'Read',
-      'none': 'None'
-    }
-  }
-  componentDidMount() {
-    this.loadMyBookShelves()
-  }
-  loadMyBookShelves = () => {
-    BooksAPI.getAll()
-      .then(books => groupItems(books, 'shelf'))
-      .then(shelves => this.setState({ shelves }))
-  }
-  handleChangeInBookShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf)
-      // .then(books => console.log('updated', books))
-      .then(() => this.loadMyBookShelves())
+    showSearchPage: false
   }
   navigateToSearch = () => {
     this.setState({ showSearchPage: true })
@@ -47,7 +26,7 @@ class BooksApp extends React.Component {
       <div className="app">
         {this.state.showSearchPage ?
           <SearchBooks onClickBack={this.navigateToShelf} /> :
-          <ListBooks {...this.state} onSearch={this.navigateToSearch} onChangeBookShelf={this.handleChangeInBookShelf} />
+          <MyReads onSearch={this.navigateToSearch} />
         }
       </div>
     )
---
+++ src/MyReads.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import ListBooks from './ListBooks'
+import { groupItems } from './Utils'
+
+class MyReads extends React.Component {
+    state = {
+        shelves: {},
+        shelfTypes: {
+            'currentlyReading': 'Currently Reading',
+            'wantToRead': 'Want to Read',
+            'read': 'Read',
+            'none': 'None'
+        }
+    }
+
+    componentDidMount() {
+        this.loadMyReads()
+    }
+
+    loadMyReads = () => {
+        BooksAPI.getAll()
+            .then(books => groupItems(books, 'shelf'))
+            .then(shelves => this.setState({ shelves }))
+
+    }
+
+    handleChangeInBookShelf = (book, shelf) => {
+        BooksAPI.update(book, shelf)
+          // .then(books => console.log('updated', books))
+          .then(() => this.loadMyReads())
+      }
+
+    render() {
+        return (
+            <ListBooks {...this.state}
+                onSearch={this.props.onSearch}
+                onChangeBookShelf={this.handleChangeInBookShelf} />
+        )
+    }
+}
+export default MyReads
