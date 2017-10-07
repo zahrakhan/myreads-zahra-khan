b7275554b8041ebb798b2cab556532d60c1e7647
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-07 14:25:45 +0500

    feat: Add deafult value option for BookShelfChanger

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,11 +1,12 @@
 export default function BookShelfChanger({
     selectedValue,
+    defaultValue,
     options,
     onChange,
 }) {
     return (
         <div className="book-shelf-changer">
-            <select value={selectedValue} onChange={event => onChange(event.target.value)}>
+            <select value={selectedValue ? selectedValue : defaultValue} onChange={event => onChange(event.target.value)}>
                 <option value="none" disabled>Move to...</option>
                 {Object.keys(options).map(option => (
                     <option key={option} value={option}>
