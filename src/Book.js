d7e9e3424ab415b2d7423302071e1b0cef64c349
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-07 15:02:10 +0500

    refactor: Make Book a stateless component
    Make Book as tateless functional component because it does not need to track any internal state

--- src/Book.js
+++ src/Book.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import BookShelfChanger from './BookShelfChanger'
 
-class Book extends Component {
-    render() {
-        const book = { ...this.props }
-        const { shelfOptions, onChangeBookShelf } = book;
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover"
-                        style={{ width: 128, height: 193, backgroundImage: `url("${book.image}")` }}>
-                    </div>
-                    <BookShelfChanger
-                        selectedValue={book.shelf}
-                        defaultValue={'none'}
-                        options={shelfOptions}
-                        onChange={onChangeBookShelf}/>
+const Book = ({
+    title,
+    authors,
+    image,
+    shelf,
+    shelfOptions,
+    onChangeBookShelf
+    }) => (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover"
+                    style={{ width: 128, height: 193, backgroundImage: `url("${image}")` }}>
                 </div>
-                <div className="book-title">{book.title || ''}</div>
-                <div className="book-authors">
-                    {book.authors && book.authors.length ? book.authors.join(', ') : 'Unknown Author'}
-                </div>
+                <BookShelfChanger
+                    selectedValue={shelf}
+                    defaultValue={'none'}
+                    options={shelfOptions}
+                    onChange={onChangeBookShelf} />
             </div>
-        )
-    }
-}
+            <div className="book-title">{title || ''}</div>
+            <div className="book-authors">
+                {authors && authors.length ? authors.join(', ') : 'Unknown Author'}
+            </div>
+        </div>
 
+    )
 export default Book
