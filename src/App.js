42f4245b14b8d5be865f90cc79034ba0f3b1cf42
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-07 15:44:57 +0500

    fix: Data validation error on intial state of shelves
    ListBooks was throwing data validation error on inital state of shelves being passed to component. Object was expected but component was receiving an empty array.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
-    shelves: [],
+    shelves: {},
     shelfTypes: {
       'currentlyReading': 'Currently Reading',
       'wantToRead': 'Want to Read',
