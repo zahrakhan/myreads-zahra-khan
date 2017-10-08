25939a7c6a2afc02fa23a5d67910f6f0eda35cce
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-08 05:01:03 +0500

    feat: Create search books bar component

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import SearchBooksBar from './SearchBooksBar'
 
 class SearchBooks extends Component {
     state = {
@@ -11,27 +11,8 @@ class SearchBooks extends Component {
     render() {
         return (
             <div className="search-books">
-                <div className="search-books-bar">
-                    <Link className="close-search" to='/'>
-                        Close
-                    </Link>
-                    <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                  <SearchBooksBar query={this.state.query} onChangeQuery={this.handleChangeInQuery}/>
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                        <input
-                            type="text"
-                            value={this.state.query}
-                            placeholder="Search by title or author"
-                            onChange={this.handleChangeInQuery}/>
-
-                    </div>
-                </div>
                 <div className="search-books-results">
                     <ol className="books-grid"></ol>
                 </div>
---
+++ src/SearchBooksBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const SearchBooksBar = ({query, onChangeQuery}) => (
+    <div className="search-books-bar">
+        <Link className="close-search" to='/'>
+            Close
+        </Link>
+        <div className="search-books-input-wrapper">
+            <input
+                type="text"
+                value={query}
+                placeholder="Search by title or author"
+                onChange={(event) => onChangeQuery(event.target.value)}/>
+
+        </div>
+    </div>
+)
+
+export default SearchBooksBar
