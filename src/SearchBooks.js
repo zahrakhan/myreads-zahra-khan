c6e9c4b06b5a3998999e67e5222028b236c4d304
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-08 06:25:52 +0500

    feat: Search for books using search query

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,17 +1,41 @@
 import React, {Component} from 'react'
+import * as BooksAPI from './BooksAPI'
 import SearchBooksBar from './SearchBooksBar'
 
 class SearchBooks extends Component {
-    state = {
-        query: ''
+    constructor(props) {
+        super(props)
+        this.state = {
+            query: '',
+            results: []
+        }
+        this.timeout = null
+    }
+    componentWillUnmount() {
+        clearTimeout(this.timeout)
     }
     handleChangeInQuery = (query) => {
-        this.setState({query})
+        this.setState({
+            query
+        }, () => this.searchForBooks())
+    }
+    searchForBooks = () => {
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(() => {
+            if (this.state.query) {
+                BooksAPI
+                    .search(this.state.query, 20)
+                    .then(results => console.log(results))
+            }
+        }, 1000)
+
     }
     render() {
         return (
             <div className="search-books">
-                  <SearchBooksBar query={this.state.query} onChangeQuery={this.handleChangeInQuery}/>
+                <SearchBooksBar
+                    query={this.state.query}
+                    onChangeQuery={this.handleChangeInQuery}/>
 
                 <div className="search-books-results">
                     <ol className="books-grid"></ol>
