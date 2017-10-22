88052eb2f279fad923dd295bc14073ee29aedc2d
ZahraKhan <zahra.khan@arbisoft.com>
2017-10-22 16:52:08 +0500

    Refactor: Move MyReads state to BooksApp component
    Move MyReads state and state manipulation to BooksApp component so that there is only one source of truth for books state. When we search for books it also returns already shelved books, i'll need to access the my books to filter out the already shevled books from search results. For that the books state needs to be in a higher order component so that both search and list books components can access it.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
-import MyReads from './MyReads'
+import * as BooksAPI from './BooksAPI'
+import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
 
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: [],
+    shelfTypes: {
+      'currentlyReading': 'Currently Reading',
+      'wantToRead': 'Want to Read',
+      'read': 'Read',
+      'none': 'None'
+    }
+  }
+
+  componentDidMount() {
+    this.loadMyReads()
+  }
+
+  loadMyReads = () => {
+    BooksAPI
+      .getAll()
+      .then(books => {
+        this.setState({books});
+        return books;
+      })
+      .catch(error => console.log('Error loading shelved books', error))
+
+  }
+
+  handleChangeInBookShelf = (book, shelf) => {
+    BooksAPI.update({
+      id: book.id
+    }, shelf)
+    .then(() => this.loadMyReads())
+    .catch(error => console.log('Error updating book status', error))
+  }
+
   render() {
     return (
       <div className='app'>
-        <Route exact path='/' render={() => (<MyReads/>)}/>
+        <Route
+          exact
+          path='/'
+          render={() => (<ListBooks {...this.state} onChangeBookShelf={this.handleChangeInBookShelf}/>)}/>
         <Route exact path="/search" render={() => (<SearchBooks/>)}/>
+
       </div>
     )
   }
